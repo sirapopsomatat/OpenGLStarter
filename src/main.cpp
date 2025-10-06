6985634d14653ba94e91eb21b6354a8348610a2b
sirapopsomatat <sirapop5847@gmail.com>
2025-10-06 14:49:29 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,6 @@
 #include <stdio.h>
 #include <string>
 #include <string.h>
-
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
@@ -21,37 +20,48 @@
 const GLint WIDTH = 800, HEIGHT = 600;
 
 Window mainWindow;
-std::vector<Mesh*> meshList;
-std::vector<Shader*> shaderList;
+std::vector<Mesh *> meshList;
+std::vector<Shader *> shaderList;
 
-//Vertex Shader
-static const char* vShader = "Shaders/shader.vert";
+// Vertex Shader
+static const char *vShader = "Shaders/shader.vert";
 
-//Fragment Shader
-static const char* fShader = "Shaders/shader.frag";
+// Fragment Shader
+static const char *fShader = "Shaders/shader.frag";
 
 void CreateTriangle()
 {
     GLfloat vertices[] =
-    {
-        -1.0f, -1.0f, 0.0f,
-        1.0f, -1.0f, 0.0f,
-        0.0f, 1.0f, 0.0f
-    };
+        {
+            -1.0f, -1.0f, 0.0f,
+            0.0f, -1.0f, 1.0f,
+            1.0f, -1.0f, 0.0f,
+            0.0f, 1.0f, 0.0f};
 
-    unsigned int indices[] = 
-    {
-        0, 1, 2,
-    };
+    unsigned int indices[] =
+        {
+            0,
+            3,
+            1,
+            1,
+            3,
+            2,
+            2,
+            3,
+            0,
+            0,
+            1,
+            2,
+        };
 
     Mesh *obj1 = new Mesh();
-    obj1->CreateMesh(vertices, indices, 9, 3);
+    obj1->CreateMesh(vertices, indices, 12, 12);
     meshList.push_back(obj1);
 }
 
 void CreateShaders()
 {
-    Shader* shader1 = new Shader();
+    Shader *shader1 = new Shader();
     shader1->CreateFromFiles(vShader, fShader);
     shaderList.push_back(shader1);
 }
@@ -64,24 +74,38 @@ int main()
     CreateTriangle();
     CreateShaders();
 
-    //Loop until window closed
+    GLuint uniformModel = 0;
+    GLuint uniformProjection = 0;
+
+    glm::mat4 projection = glm::perspective(45.0f, (GLfloat)mainWindow.getBufferWidth() / (GLfloat)mainWindow.getBufferHeight(), 0.1f, 100.0f);
+
+    // Loop until window closed
     while (!mainWindow.getShouldClose())
     {
-        //Get + Handle user input events
+        // Get + Handle user input events
         glfwPollEvents();
 
-        //Clear window
+        // Clear window
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        //draw here
+        // draw here
         shaderList[0]->UseShader();
+        uniformModel = shaderList[0]->GetUniformLocation("model");
+        uniformProjection = shaderList[0]->GetUniformLocation("projection");
 
-        //Object
+        // Object
+        glm::mat4 model(1.0f);
+        model = glm::translate(model, glm::vec3(0.3f, 0.0f, -2.5f));
+        // model = glm::rotate(model, glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+        model = glm::scale(model, glm::vec3(0.4f, 0.4f, 1.0f));
+
+        glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+        glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
         meshList[0]->RenderMesh();
 
         glUseProgram(0);
-        //end draw
+        // end draw
 
         mainWindow.swapBuffers();
     }
