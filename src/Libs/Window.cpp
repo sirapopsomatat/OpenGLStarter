8b3d6c0d2a4671d3c596ebd27c1d216199eb4f91
sirapopsomatat <sirapop5847@gmail.com>
2025-10-13 22:55:43 +0700

    Update Window.cpp

--- src/Libs/Window.cpp
+++ src/Libs/Window.cpp
@@ -24,47 +24,56 @@ Window::~Window()
 
 int Window::initialise()
 {
-    //init GLFW
+    // Register an error callback so we get useful messages on failure
+    static auto glfw_error_callback = [](int error, const char *description)
+    {
+        fprintf(stderr, "GLFW Error (%d): %s\n", error, description ? description : "(null)");
+    };
+    glfwSetErrorCallback(glfw_error_callback);
+
+    // init GLFW
     if (!glfwInit())
     {
-        printf("GLFW initialisation failed!");
+        fprintf(stderr, "GLFW initialisation failed!\n");
         glfwTerminate();
         return 1;
     }
 
-    //Setup GLFW window properties
-    //OpenGL version (using 3.1)
+    // Setup GLFW window properties
+    // OpenGL version (using 3.1)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, glfwMajorVersion);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, glfwMinorVersion);
 
-    //Core Profile = No Backwards Compatibility
-    glfwWindowHint(GLFW_OPENGL_ANY_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // Core Profile = No Backwards Compatibility
+    //  Use the standard profile hint (don't use a non-standard ANY_PROFILE hint)
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    #ifdef __APPLE__
-        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    #endif
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
 
     mainWindow = glfwCreateWindow(width, height, "Test Window", NULL, NULL);
 
     if (!mainWindow)
     {
-        printf("GLFW window creation failed!");
+        fprintf(stderr, "GLFW window creation failed!\n");
         glfwTerminate();
         return 1;
     }
 
-    //Get Buffer size information
+    // Get Buffer size information
     glfwGetFramebufferSize(mainWindow, &bufferWidth, &bufferHeight);
 
-    //Set context for GLEW to use
+    // Set context for GLEW to use
     glfwMakeContextCurrent(mainWindow);
+    // Ensure the window is shown (some platforms or flags may hide it)
+    glfwShowWindow(mainWindow);
 
-    //Allow modern extension features
+    // Allow modern extension features
     glewExperimental = GL_TRUE;
 
     if (glewInit() != GLEW_OK)
     {
-        printf("GLEW initialisation failed!");
+        fprintf(stderr, "GLEW initialisation failed!\n");
         glfwDestroyWindow(mainWindow);
         glfwTerminate();
         return 1;
@@ -72,7 +81,7 @@ int Window::initialise()
 
     glEnable(GL_DEPTH_TEST);
 
-    //Setup Viewport size
+    // Setup Viewport size
     glViewport(0, 0, bufferWidth, bufferHeight);
 
     return 0;
