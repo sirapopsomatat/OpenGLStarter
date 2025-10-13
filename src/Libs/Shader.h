f95fcba3b4750f0e4c7d6216abb059ac1972f043
sirapopsomatat <sirapop5847@gmail.com>
2025-10-13 22:55:41 +0700

    Update Shader.h

--- src/Libs/Shader.h
+++ src/Libs/Shader.h
@@ -11,24 +11,23 @@
 
 class Shader
 {
-    public:
-        Shader();
-        ~Shader();
+public:
+    Shader();
+    ~Shader();
 
-        void CreateFromString (const char* vertexCode, const char* fragmentCode);
-        void CreateFromFiles (const char* vertexLocation, const char* fragmentLocation);
-        std::string ReadFile(const char* fileLocation);
+    void CreateFromString(const char *vertexCode, const char *fragmentCode);
+    void CreateFromFiles(const char *vertexLocation, const char *fragmentLocation);
+    std::string ReadFile(const char *fileLocation);
 
-        void UseShader();
-        void ClearShader();
+    void UseShader();
+    void ClearShader();
 
-        GLuint GetUniformLocation(const char* uniformName) {return glGetUniformLocation(shader, uniformName);}
+    GLuint GetUniformLocation(const char *uniformName) { return glGetUniformLocation(shader, uniformName); }
 
-    private:
-        GLuint shader;
-        void CompileShaders(const char* vertexCode, const char* fragmentCode);
-        void AddShader(GLuint theProgram, const char* shaderCode, GLenum shaderType);
-
+private:
+    GLuint shader;
+    void CompileShaders(const char *vertexCode, const char *fragmentCode);
+    void AddShader(GLuint theProgram, const char *shaderCode, GLenum shaderType);
 };
 
 #endif
